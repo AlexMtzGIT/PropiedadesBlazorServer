7f35829a91eb7075ffde47301105d8132e3c0047
alejandro.martinezal <alejandro.martinezal@CMMXLP-01583>
2026-04-14 15:26:48 -0600

    Se creó el modelo Categoria, se agrego en el ApplicationDbContext y se configuro la cadena de conexion (en este caso con (Microsoft SQL Local DB)

--- PropiedadesBlazor/Data/ApplicationDbContext.cs
+++ PropiedadesBlazor/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PropiedadesBlazor.Modelos;
 
 namespace PropiedadesBlazor.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        //agregar modelos
+        public DbSet<Categoria> Categorias { get; set; }
     }
 }
